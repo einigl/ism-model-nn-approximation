Fix derivatives

nnbma/layers/additional_module.py
@@ -24,7 +24,6 @@ class AdditionalModule(nn.Module, ABC):
         device: str = "cpu",
     ):
         r"""
-
         Parameters
         ----------
         input_features: int
@@ -40,10 +39,13 @@ class AdditionalModule(nn.Module, ABC):
         self.output_features = output_features
         self.device = device
 
+        if module is not None:
+            self.forward = module.forward
+
     @abstractmethod
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         r"""
-        Evaluates the associated pytorch function.
+        Evaluates the associated PyTorch function.
 
         Parameters
         ----------

nnbma/layers/restrictable_linear.py
@@ -86,13 +86,12 @@ class RestrictableLinear(nn.Linear):
             self.subbias = self.bias.data[indices]
 
     def train(self, mode: bool = True) -> "RestrictableLinear":
-        r"""Sets the object in train mode if ``mode==True``and in eval mode else.
-
+        r"""Sets the object in train mode if ``mode is True`` and in eval mode else.
 
         Parameters
         ----------
         mode : bool, optional
-            wether the layer is to be set in train mode (``True``) or eval mode (``False``), by default ``True``.
+            Whether the layer is to be set in train mode (``True``) or eval mode (``False``), by default ``True``.
 
         Returns
         -------

nnbma/networks/densely_connected.py
@@ -80,7 +80,6 @@ class DenselyConnected(NeuralNetwork):
         self.layers_sizes = [input_features]
         for k in range(n_layers - 1):
             n_outputs = ceil(growing_factor * n_inputs)
-            self.layers_sizes.append(n_outputs)
 
             if batch_norm and k < n_layers - 2:
                 self.layers.append(
@@ -93,8 +92,7 @@ class DenselyConnected(NeuralNetwork):
                 self.layers.append(nn.Linear(n_inputs, n_outputs, device=self.device))
 
             n_inputs += n_outputs
-
-        # self.layers.to(self.device)
+            self.layers_sizes.append(n_inputs)
 
         if last_restrictable:
             self.output_layer = RestrictableLinear(
@@ -110,10 +108,6 @@ class DenselyConnected(NeuralNetwork):
                 device=self.device,
             )
         self.layers_sizes.append(output_features)
-
-        # print('layers_sizes')
-        # print(self.layers_sizes)
-        # print(sum(self.layers_sizes[:-1]))
 
     def forward(self, x: Tensor) -> Tensor:
         xk = x.clone()

tests/layers/test_polynomial_expansion.py
@@ -1,5 +1,5 @@
-import numpy as np
 import torch
+from torch.func import jacrev, vmap
 
 from nnbma.layers.polynomial_expansion import PolynomialExpansion
 
@@ -20,5 +20,21 @@ def test_forward():
     x = torch.ones((1, n_dim_in))
     y = poly_exp.forward(x)
 
-    assert y.shape == (1, 9)
-    assert np.allclose(y, torch.ones((1, 9)))
+    n_poly = 9
+    assert poly_exp.n_expanded_features == n_poly
+    assert y.shape == (1, n_poly)
+    assert torch.allclose(y, torch.ones((1, n_poly)))
+
+
+def test_diff():
+    n_dim_in = 2
+    order = 2
+    poly_exp = PolynomialExpansion(n_dim_in, order)
+    dpoly_exp = vmap(jacrev(poly_exp))
+
+    x = torch.tensor([[1, 2]], dtype=torch.float32)
+    dy = dpoly_exp(x).squeeze(0)
+
+    assert torch.allclose(
+        dy, torch.tensor([[1, 0], [0, 1], [2, 0], [2, 1], [0, 4]], dtype=dy.dtype)
+    )